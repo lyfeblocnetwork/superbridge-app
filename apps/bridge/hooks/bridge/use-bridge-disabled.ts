0600f897d86e90dc794805da9044e5a8984daa0a
Alex Harley <alex@alexbh.dev>
2024-10-31 08:26:28 +0100

    chore: undisable base

--- apps/bridge/hooks/bridge/use-bridge-disabled.ts
+++ apps/bridge/hooks/bridge/use-bridge-disabled.ts
@@ -1,13 +1,3 @@
-import { useDeployment } from "../deployments/use-deployment";
-import {
-  useIsOptimismForcedWithdrawal,
-  useIsOptimismWithdrawal,
-} from "../use-withdrawing";
-
 export const useBridgeDisabled = () => {
-  const isWithdrawal = useIsOptimismWithdrawal();
-  const isForcedWithdrawal = useIsOptimismForcedWithdrawal();
-  const deployment = useDeployment();
-
-  return (isWithdrawal || isForcedWithdrawal) && deployment?.name === "base";
+  return false;
 };
