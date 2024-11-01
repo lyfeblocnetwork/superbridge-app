ac65d5b2c5c7d15baae1dc54d6ee338413ca74a1
Alex Harley <alex@alexbh.dev>
2024-11-01 14:13:27 +0100

    fix: some tokens are bridgeable one way

--- apps/bridge/utils/is-bridgeable.ts
+++ apps/bridge/utils/is-bridgeable.ts
@@ -1,5 +1,5 @@
 import { BridgeableTokenDto } from "@/codegen/model";
 
 export function isBridgeable(a: BridgeableTokenDto, b: BridgeableTokenDto) {
-  return a.bridges.includes(b.chainId) && b.bridges.includes(a.chainId);
+  return a.bridges.includes(b.chainId);
 }
