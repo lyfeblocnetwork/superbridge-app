d834df289ca7dded9574877c4b45ac1a7c815b8b
Alex Harley <alex@alexbh.dev>
2024-11-04 18:29:55 +0100

    fix: transfer time on withdrawals

--- apps/bridge/hooks/activity/use-tx-duration.ts
+++ apps/bridge/hooks/activity/use-tx-duration.ts
@@ -1,10 +1,12 @@
 import { Transaction } from "@/types/transaction";
 import {
   isAcrossBridge,
+  isArbitrumWithdrawal,
   isCctpBridge,
   isDeposit,
   isHyperlaneBridge,
   isLzBridge,
+  isOptimismWithdrawal,
 } from "@/utils/guards";
 
 import { useTxDeployment } from "./use-tx-deployment";
@@ -15,6 +17,11 @@ export const useTxDuration = (
   const deployment = useTxDeployment(tx);
   if (!tx) return undefined;
   if (isDeposit(tx)) return deployment?.depositDuration;
+  if (isOptimismWithdrawal(tx))
+    return (
+      (deployment?.proveDuration ?? 0) + (deployment?.finalizeDuration ?? 0)
+    );
+  if (isArbitrumWithdrawal(tx)) return deployment?.finalizeDuration;
   if (isAcrossBridge(tx)) return tx.duration;
   if (isHyperlaneBridge(tx)) return tx.duration;
   if (isCctpBridge(tx)) return tx.duration;
