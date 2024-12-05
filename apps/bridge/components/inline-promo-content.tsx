6da5d80db9f34ebc9d0d664bd33e4ce4cef6fe88
Alex Harley <alex@alexbh.dev>
2024-12-05 16:26:16 +0100

    fix: widget should not have inline promo content

--- apps/bridge/components/inline-promo-content.tsx
+++ apps/bridge/components/inline-promo-content.tsx
@@ -1,7 +1,6 @@
 import { useIsHyperlanePlayground } from "@/hooks/apps/use-is-hyperlane";
 import { useIsLzPlayground } from "@/hooks/apps/use-is-lz";
 import { useIsWidget } from "@/hooks/use-is-widget";
-import { useDeletedAt } from "@/hooks/use-metadata";
 
 import { HLContent } from "./inline-promo-content/HLcontent";
 import { LZContent } from "./inline-promo-content/LZcontent";
@@ -10,6 +9,11 @@ import { Footer } from "./inline-promo-content/footer";
 export const InlinePromoContent = () => {
   const isHyperlane = useIsHyperlanePlayground();
   const isLz = useIsLzPlayground();
+  const isWidget = useIsWidget();
+
+  if (isWidget) {
+    return null;
+  }
 
   if (isHyperlane) {
     return (
