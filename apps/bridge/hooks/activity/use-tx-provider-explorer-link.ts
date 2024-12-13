f7c2ccaee35aea169c6f770f08e71ab65962a56b
Alex Harley <alex@alexbh.dev>
2024-12-13 17:28:02 +0100

    feat: layerzeroscan link

--- apps/bridge/hooks/activity/use-tx-provider-explorer-link.ts
+++ apps/bridge/hooks/activity/use-tx-provider-explorer-link.ts
@@ -1,11 +1,15 @@
 import { Transaction } from "@/types/transaction";
-import { isHyperlaneBridge } from "@/utils/guards";
+import { isHyperlaneBridge, isLzBridge } from "@/utils/guards";
 
 export const useTxProviderExplorerLink = (
   tx: Transaction | null | undefined
 ) => {
   if (tx && isHyperlaneBridge(tx))
     return `https://explorer.hyperlane.xyz/message/${tx.hyperlaneMessageId}`;
+
+  if (tx && isLzBridge(tx)) {
+    return `https://layerzeroscan.com/tx/${tx.send.transactionHash}`;
+  }
 
   return null;
 };
